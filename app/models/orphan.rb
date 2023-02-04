de560acf999ee744847d2e5130fca1078ee77a43
IBRAHIMA <hadi.ibrahima8@gmail.com>
2023-02-04 08:39:52 +0100

    new one

--- app/models/orphan.rb
+++ app/models/orphan.rb
@@ -2,7 +2,8 @@ class Orphan < ApplicationRecord
   #belongs_to :hostparent
   #belongs_to :originalparent
   #belongs_to :user
-validates :name, :description, null:false, presence: true, length: { minimum: 1 }
+validates :name, :description, null:false, presence: true, length: { minimum: 1 }, unless: :image?
 validates :age, :sex, presence: false
+mount_uploader :image, ImageUploader
 #paginates_per 10
 end
